6289b4f226e344d2314b63a0c6eafe2ce225ffac
tasmidur <tasmidurrahman@gmail.com>
2022-06-11 22:19:41 +0600

    sink-data-mysql-to-mongodb

--- event-config.js
+++ event-config.js
@@ -4,18 +4,22 @@ module.exports = {
      * Pass true to only emit binlog events that occur after ZongJi's instantiation.
      * Must be used in start() method for effect.
      */
-    startAtEnd: true,
+    startAtEnd: false,
     /**
      * Databases and tables to include (Only for row events).
      * Use database names as the key and pass an array of table names or true (for the entire database)
      * Example: { 'my_database': ['allow_table', 'another_table'], 'another_db': true }
      */
-    includeSchema: {},
+    includeSchema: {
+        cd:true,
+        cdc:true
+    },
     /**
      * Object describing which databases and tables to exclude (Same format as includeSchema)
      * Example: { 'other_db': ['disallowed_table'], 'ex_db': true }
      */
     excludeSchema: {
-        mysql: true
+        mysql: true,
+        sys:true
     }
 }
